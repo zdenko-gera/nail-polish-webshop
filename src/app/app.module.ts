416eaa7e163d1f86ed50a2bbb4be9d36967c49a7
Gera Zdenkó <h157872@stud.u-szeged.hu>
2024-04-21 18:37:30 +0200

    Alap oldalak és menusor komponens hozzáadva

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { StoresComponent } from './pages/stores/stores.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ContactComponent,
+    StoresComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
